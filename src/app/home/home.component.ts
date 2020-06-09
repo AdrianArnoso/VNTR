ca6279d5eaf907b8c8e24ca973666dd70e5027a0
Arnoso Bello <V364972@CAIXAGALICIA.CG>
2020-06-09 16:40:38 +0200

    Home OK sin seleccion por click

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface Servicios {
+  name: string;
+  id: string;
+  descripcion: string;
+  criticidad: string;
+  responsable: string;
+  negocio: string;
+}
+
+const ELEMENT_DATA: Servicios[] = [
+  {id: "STI01", name: 'Desarrollo y Mantenimiento de Software', descripcion: "", criticidad: 'B', responsable: "Arnoso", negocio: "ABANCA España"},
+  {id: "STI02", name: 'Implantación y Configuración de Productos', descripcion: "", criticidad: 'C', responsable: "Quijano", negocio: "ABANCA USA"},
+  {id: "STI03", name: 'Instalación y retirada de equipamiento TIC', descripcion: "", criticidad: 'C', responsable: "Fraga", negocio: "ABANCA Portugal"},
+  
+];
 
 @Component({
   selector: 'abanca-home',
@@ -12,6 +29,13 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
+  displayedColumns: string[] = ['id', 'name', 'descripcion', 'criticidad', 'responsable', 'negocio'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
 
 
