5e0e73d2c65c6c8d55239de98b8f4239926cd428
Arnoso Bello <V364972@CAIXAGALICIA.CG>
2020-06-11 10:52:55 +0200

    Home con botones search

--- src/app/sistemas/sistemas.component.ts
+++ src/app/sistemas/sistemas.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild,} from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'abanca-sistemas',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sistemas.component.scss']
 })
 export class SistemasComponent implements OnInit {
+  columnas: string[] = ['id','sistemaInformacion','criticidad','borrar'];
 
-  constructor() { }
+  @ViewChild(MatSort) sort: MatSort;
 
-  ngOnInit() {
+  dataSource = null;
+
+  datos: Sistema [] = [
+    {id: 'SI00001', sistemaInformacion: 'Sistema de información patrimonial', criticidad: 'No'},
+    {id: 'SI00002', sistemaInformacion: 'Sistema de información CRM operativo', criticidad: 'No'},
+    {id: 'SI00003', sistemaInformacion: 'Sistema de información CRM analítico', criticidad: 'No'},
+    {id: 'SI00004', sistemaInformacion: 'Sistema de información de comunicación al empleado', criticidad: 'No'},
+  ];
+  constructor() { 
+    
   }
 
+  ngOnInit() {
+   this.dataSource = new MatTableDataSource<Sistema>(this.datos);
+   this.dataSource.sort = this.sort;
+  }
+
+}
+
+export class Sistema {
+  constructor(public id: string, public sistemaInformacion: string, public criticidad: string){
+
+
+  }
 }
--- src/app/sistemas/sistemas.module.ts
+++ src/app/sistemas/sistemas.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { SistemasRoutingModule } from './sistemas-routing.module';
 import { SistemasComponent } from './sistemas.component';
+import { MatSortModule, MatButtonModule, MatTableModule, MatToolbarModule, MatTabsModule, MatSidenavModule, } from '@angular/material';
+
 
 @NgModule({
   imports: [
     CommonModule,
-    SistemasRoutingModule
+    SistemasRoutingModule,
+    MatSortModule,
+    MatTableModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatSidenavModule
   ],
   declarations: [SistemasComponent]
 })
