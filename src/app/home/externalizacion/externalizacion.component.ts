7fe89764f1d0b2adb658aae8d3b8d04147dec270
Regueira Souto <V364973@CAIXAGALICIA.CG>
2020-06-12 11:51:42 +0200

    sort externalizacion

--- src/app/home/externalizacion/externalizacion.component.ts
+++ src/app/home/externalizacion/externalizacion.component.ts
@@ -1,25 +1,26 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatSort } from '@angular/material';
+import {  MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
-export interface Extarnilacion {
-  Se: String;
-  Id_Actividad: string;
-  Descripcion: string;
-  Externalizacion: string;
-  Criticidad:string;
-  Altia:string;
-  Indra: string;
-}
-
-
-
+  //  export interface Extarnilacion {
+  //   Se: String;
+  //   Id_Actividad: string;
+  //   Descripcion: string;
+  //   Externalizacion: string;
+  //   Criticidad:string;
+  //   Altia:string;
+  //   Indra: string;
+  //  }
 
 
-const ELEMENT_DATA02: Extarnilacion[] = [
-  {Se: '0', Id_Actividad: 'ST01', Descripcion: 'Servicios de desarrollo de nuevas soluciones',Externalizacion: 'SI',Criticidad:'B',Altia: 'X',Indra: 'X'},
-  {Se: '0', Id_Actividad: 'ST01', Descripcion: 'Servicios de desarrollo derivados de cambios',Externalizacion: 'SI',Criticidad:'A',Altia: '',Indra: 'X'}
-  
-];
+
+
+
+  // const ELEMENT_DATA02: Extarnilacion[] = [
+  //   {Se: '0', Id_Actividad: 'ST01', Descripcion: 'Servicios de desarrollo de nuevas soluciones',Externalizacion: 'SI',Criticidad:'B',Altia: 'X',Indra: 'X'},
+  //   {Se: '0', Id_Actividad: 'ST01', Descripcion: 'Servicios de desarrollo derivados de cambios',Externalizacion: 'SI',Criticidad:'A',Altia: '',Indra: 'X'}
+    
+  // ];
 
 @Component({
   selector: 'abanca-externalizacion',
@@ -29,12 +30,30 @@ const ELEMENT_DATA02: Extarnilacion[] = [
 export class ExternalizacionComponent implements OnInit {
 
   displayedColumns: string[] = ['Se', 'Id_Actividad', 'Descripcion', 'Externalizacion','Criticidad','Altia','Indra'];
-  dataSource = ELEMENT_DATA02;
+  // dataSource = ELEMENT_DATA02;
   constructor() { }
-
-  @ViewChild(MatSort) sort: MatSort;
   
+  
+   @ViewChild(MatSort) sort: MatSort;
+
+     datos: Externalizacion[] = [
+    {Se: '0', Id_Actividad: 'ST01', Descripcion: 'Servicios de desarrollo de nuevas soluciones',Externalizacion: 'SI',Criticidad:'B',Altia: 'X',Indra: 'X'},
+    {Se: '0', Id_Actividad: 'ST01', Descripcion: 'Servicios de desarrollo derivados de cambios',Externalizacion: 'SI',Criticidad:'A',Altia: '',Indra: 'X'}
+    
+   ];
+   
+
+
+  dataSource = new MatTableDataSource<Externalizacion>(this.datos);
   ngOnInit() {
+    this.dataSource.sort = this.sort;
   }
+}
+
+  export class Externalizacion {
+    constructor(public Se: string, public Id_Actividad: string, public Descripcion: string, public Externalizacion: string, public Criticidad: string, public Altia: string, public Indra: string){
+  
+  
+    }
 
 }
