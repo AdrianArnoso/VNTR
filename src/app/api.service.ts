f9b8f039a6e9bd672fb6342e43dfd9425fb9d793
Regueira Souto <V364973@CAIXAGALICIA.CG>
2020-06-29 17:40:03 +0200

    actualizar post

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -8,13 +8,18 @@ import { map } from 'rxjs/operators';
 export class ApiService {
 
   private path = 'https://localhost:5001/Adrian';
+  URL_ACTIVIDADE_EXTERNALIZACION ="https://localhost:5001/postActividadExternalizacion";
 
   constructor(private http: HttpClient) { }
 
   getServicios$() {
     return this.http.get<any[]>(this.path).pipe(map(data => (data ? data : [])));
   }
-
+  postActividadExternalizacion(datos){
+    console.log(datos);
+    const url = this.URL_ACTIVIDADE_EXTERNALIZACION;
+    return this.http.post<any>(url,datos);  
+  }
 
 
 }
