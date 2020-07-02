57503153f93d2e4394e2a8f8d182d12529fc3ea5
AdrianArnoso <aarnoso@oesia.com>
2020-07-02 14:42:12 +0200

    mat-select rellenados automat

--- src/app/home/form-modify-catalogoservicios/form-modify-catalogoservicios.component.ts
+++ src/app/home/form-modify-catalogoservicios/form-modify-catalogoservicios.component.ts
@@ -21,13 +21,20 @@ export class FormModifyCatalogoserviciosComponent implements OnInit {
   @Input() responsables;
   @Input() servicios;
 
-  selected = "c";
+  selectedCrit = "";
+  selectedResp = "";
+  selectedNeg = "";
+
 
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     console.log(this.servicios);
+    this.selectedCrit = this.servicios[0].criticidad.toLowerCase();
+    this.selectedResp = this.servicios[0].responsable;
+    this.selectedNeg = this.servicios[0].negocio;
+    console.log("Selected " + this.selectedNeg );
   }
 
 
