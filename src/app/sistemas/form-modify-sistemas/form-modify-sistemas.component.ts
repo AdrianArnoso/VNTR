4b4e560bc6b3c298adf7e1538822a0fb1218f166
Prego Piñeiro <V364974@CAIXAGALICIA.CG>
2020-07-06 13:12:54 +0200

    formulario modificar sistemas

---
+++ src/app/sistemas/form-modify-sistemas/form-modify-sistemas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'abanca-form-modify-sistemas',
+  templateUrl: './form-modify-sistemas.component.html',
+  styleUrls: ['./form-modify-sistemas.component.scss']
+})
+export class FormModifySistemasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
