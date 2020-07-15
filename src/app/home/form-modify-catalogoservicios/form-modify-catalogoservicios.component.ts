3f96927eb73d9411ad5ca71c45ba54645e3fd82f
Prego Piñeiro <V364974@CAIXAGALICIA.CG>
2020-07-15 11:53:17 +0200

    form mod servicio - validadores y botón funciona

--- src/app/home/form-modify-catalogoservicios/form-modify-catalogoservicios.component.ts
+++ src/app/home/form-modify-catalogoservicios/form-modify-catalogoservicios.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Validators, FormBuilder } from '@angular/forms';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { Location } from '@angular/common'
 
 
@@ -10,15 +10,17 @@ import { Location } from '@angular/common'
 
 })
 export class FormModifyCatalogoserviciosComponent implements OnInit {
-  catalogoForm = this.fb.group({
-    catalogo_servicios_es: [null, [Validators.required]],
-    catalogo_servicios_en: [null, [Validators.required]],
-    responsable: [null, [Validators.required]],
-    negocio: [null, [Validators.required]],
-    descripcion_es: [null, [Validators.required]],
-    descripcion_en: [null, [Validators.required]],
-    criticidad: [null, [Validators.required]]
-  });
+catalogoForm : FormGroup;
+
+  // catalogoForm = this.fb.group({
+  //   catalogo_servicios_es: [null, [Validators.required]],
+  //   catalogo_servicios_en: [null, [Validators.required]],
+  //   responsable: [null, [Validators.required]],
+  //   negocio: [null, [Validators.required]],
+  //   descripcion_es: [null, [Validators.required]],
+  //   descripcion_en: [null, [Validators.required]],
+  //   criticidad: [null, [Validators.required]]
+  // });
 
   @Input() negocios;
   @Input() responsables;
@@ -27,19 +29,35 @@ export class FormModifyCatalogoserviciosComponent implements OnInit {
   selectedCrit = "";
   selectedResp = "";
   selectedNeg = "";
+  dataCatalogo ="";
+  dataDescripcion ="";
 
 
 
   constructor(private fb: FormBuilder,
     private location: Location
-    ) { }
+    ) {
+
+     }
 
   ngOnInit() {
     console.log(this.servicios);
     this.selectedCrit = this.servicios[0].criticidad.toLowerCase();
     this.selectedResp = this.servicios[0].responsable;
     this.selectedNeg = this.servicios[0].negocio;
+    this.dataCatalogo = this.servicios[0].name;
+    this.dataDescripcion = this.servicios[0].descripcion;
     console.log("Selected " + this.selectedNeg );
+
+    this.catalogoForm = this.fb.group({
+      catalogo_servicios_es: [this.dataCatalogo, [Validators.required]],
+      catalogo_servicios_en: ['', [Validators.required]],
+      responsable: [this.selectedResp, [Validators.required]],
+      negocio: [this.selectedNeg, [Validators.required]],
+      descripcion_es: [this.dataDescripcion, [Validators.required]],
+      descripcion_en: ['', [Validators.required]],
+      criticidad: [this.selectedCrit, [Validators.required]]
+    });
   }
 
 
